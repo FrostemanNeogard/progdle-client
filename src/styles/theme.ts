b634d6f52f200717aa7ac46b84431aa0b250a073
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-16 10:52:05 +0200

    add additional colors

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,13 +3,17 @@ export const theme = {
     md: "768px",
   },
   colors: {
+    success: "#48ff38",
+    warning: "#ffff54",
+    danger: "#fe5353",
     bg: {
       primary: "#343434",
       secondary: "#282828",
+      white: "#ffffff",
     },
     tx: {
       primary: "#f7f7f7",
-      secondary: "7f7f7f",
-    }
+      secondary: "#000000",
+    },
   },
 };
