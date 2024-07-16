dfeba0fc150fde1f772fec186cc0db0a1104cd46
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-16 10:54:00 +0200

    linting

--- src/components/GuessInputBar.styled.ts
+++ src/components/GuessInputBar.styled.ts
@@ -9,15 +9,15 @@ export const GuessForm = styled.form`
 
 export const GuessInput = styled.input`
   width: 65%;
-  background-color: ${props => props.theme.colors.bg.white};
+  background-color: ${(props) => props.theme.colors.bg.white};
   border-radius: 9999px;
   padding: 0.5rem 1rem;
 `;
 
 export const GuessButton = styled.button`
   width: 30%;
-  background-color: ${props => props.theme.colors.success};
+  background-color: ${(props) => props.theme.colors.success};
   border: none;
-  color: ${props => props.theme.colors.tx.secondary};
+  color: ${(props) => props.theme.colors.tx.secondary};
   border-radius: 9999px;
 `;
