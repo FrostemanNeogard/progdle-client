bfcf95dd9069ba175a328b10bbfd3653e381071d
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-16 10:53:38 +0200

    improve styling

--- src/pages/root/RootPage.styled.ts
+++ src/pages/root/RootPage.styled.ts
@@ -1,10 +1,19 @@
 import styled from "styled-components";
 
 export const Root = styled.div`
-  background-color: ${props => props.theme.colors.bg.primary};
-  color: ${props => props.theme.colors.tx.primary};
+  background-color: ${(props) => props.theme.colors.bg.primary};
+  color: ${(props) => props.theme.colors.tx.primary};
+  font-family: "Montserrat";
   min-height: 100vh;
   color-scheme: dark;
+  display: flex;
+  flex-direction: column;
 `;
 
-export const MainContent = styled.main``;
+export const MainContent = styled.main`
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  flex: 1;
+  padding: 1rem;
+`;
