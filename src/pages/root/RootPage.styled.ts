7d51ee8ea4b39dc834e89f7aaf56de2ab7a3bcb4
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-16 08:58:03 +0200

    create RootPage component

---
+++ src/pages/root/RootPage.styled.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Root = styled.div`
+  background-color: #3e3e40;
+  min-height: 100vh;
+  color-scheme: dark;
+`;
+
+export const MainContent = styled.main``;
