c89bf2016c0f6cbb2549148cf9cf0fb3d644332f
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-16 09:34:27 +0200

    add text colors and slightly change overall structure

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,7 +3,13 @@ export const theme = {
     md: "768px",
   },
   colors: {
-    primary: "#282828",
-    secondary: "#343434",
+    bg: {
+      primary: "#343434",
+      secondary: "#282828",
+    },
+    tx: {
+      primary: "#f7f7f7",
+      secondary: "7f7f7f",
+    }
   },
 };
