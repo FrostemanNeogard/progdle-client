2c53ce1c29d391c8ce30c5f5de5ea28a643f46e4
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-16 09:23:24 +0200

    add ThemeProvider to root

--- src/pages/root/RootPage.tsx
+++ src/pages/root/RootPage.tsx
@@ -1,14 +1,18 @@
 import { Outlet } from "@tanstack/react-router";
 import * as S from "./RootPage.styled";
 import AppHeader from "../../components/AppHeader";
+import { ThemeProvider } from "styled-components";
+import { theme } from "../../styles/theme";
 
 export default function RootPage() {
   return (
-    <S.Root>
-      <AppHeader />
-      <S.MainContent>
-        <Outlet />
-      </S.MainContent>
-    </S.Root>
+    <ThemeProvider theme={theme}>
+      <S.Root>
+        <AppHeader />
+        <S.MainContent>
+          <Outlet />
+        </S.MainContent>
+      </S.Root>
+    </ThemeProvider>
   );
 }
