08159b1f3f06d2aecb34d0d408c1166d4737550a
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-17 09:22:35 +0200

    improve styling

--- src/components/GuessInputBar.styled.ts
+++ src/components/GuessInputBar.styled.ts
@@ -8,7 +8,7 @@ export const GuessForm = styled.form`
 
   input,
   button {
-    font-size: 2rem;
+    font-size: 1.5rem;
   }
 `;
 
@@ -16,12 +16,14 @@ export const GuessInput = styled.input`
   flex: 1;
   background-color: ${(props) => props.theme.colors.bg.white};
   border-radius: 9999px;
-  padding: 0.5rem 1rem;
+  padding: 1rem 2rem;
+  color: ${(props) => props.theme.colors.tx.secondary};
 `;
 
 export const GuessButton = styled.button`
   width: 30%;
   background-color: ${(props) => props.theme.colors.success};
+  padding-inline: 1rem;
   border: none;
   color: ${(props) => props.theme.colors.tx.secondary};
   border-radius: 9999px;
