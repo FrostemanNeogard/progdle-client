84f71c403a6ce3bbe155ba03f0f7604682d1e794
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-17 13:58:10 +0200

    add snippet data handling

--- src/components/CodeSnippet.tsx
+++ src/components/CodeSnippet.tsx
@@ -1,13 +1,36 @@
 import SyntaxHighlighter from "react-syntax-highlighter";
 import * as S from "./CodeSnippet.styled.ts";
+import { useQuery } from "@tanstack/react-query";
+import { useContext } from "react";
+import { GuessesContext } from "../pages/root/RootPage.tsx";
+import axios from "axios";
 
-type CodeSnippetProps = {
-  content: string;
-};
-export default function CodeSnippet({ content }: CodeSnippetProps) {
+export default function CodeSnippet() {
+  const { guesses } = useContext(GuessesContext);
+  const { isPending, error, data } = useQuery({
+    queryKey: ["snippet-level-" + guesses.length],
+    queryFn: () => axios.get("http://localhost:8080/api/snippets/daily/" + (guesses.length + 1)),
+  })
+
+  if (isPending) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    )
+  }
+
+  if (error) {
+    return (
+      <div>
+        <h1>Error: {error.message}</h1>
+      </div>
+    )
+  }
+
   return (
     <S.CodeBlock>
-      <SyntaxHighlighter language="javascript">{content}</SyntaxHighlighter>
+      <SyntaxHighlighter language="javascript">{data.data.content}</SyntaxHighlighter>
     </S.CodeBlock>
   );
 }
