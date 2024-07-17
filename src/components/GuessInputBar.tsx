40244ca8e928c0b3632ea3ec88b5381dffe0950b
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-17 09:23:20 +0200

    begin integrating API for making guesses

--- src/components/GuessInputBar.tsx
+++ src/components/GuessInputBar.tsx
@@ -1,12 +1,26 @@
-import { SyntheticEvent, useState } from "react";
+import { SyntheticEvent, useContext, useState } from "react";
 import * as S from "./GuessInputBar.styled";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { GuessesContext } from "../pages/root/RootPage";
+import { Guess } from "../types/Guess";
 
 export default function GuessInputBar() {
   const [languageInput, setLanguageInput] = useState<string>("");
+  const { addGuess } = useContext(GuessesContext);
+
+  const { isPending, data, mutate } = useMutation({
+    mutationFn: (guess: string) => {
+      return axios.get("http://localhost:8080/api/guess/" + guess);
+    },
+    onSuccess: (res: { data: Guess }) => {
+      addGuess(res.data);
+    },
+  });
 
   const submitGuess = (e: SyntheticEvent) => {
     e.preventDefault();
-    console.log("Making guess...");
+    mutate(languageInput);
   };
 
   return (
@@ -17,7 +31,9 @@ export default function GuessInputBar() {
         value={languageInput}
         onChange={(e) => setLanguageInput(e.target.value)}
       />
-      <S.GuessButton>Guess</S.GuessButton>
+      <S.GuessButton disabled={isPending} onClick={submitGuess}>
+        Guess
+      </S.GuessButton>
     </S.GuessForm>
   );
 }
