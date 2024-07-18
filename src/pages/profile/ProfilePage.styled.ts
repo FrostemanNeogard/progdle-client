08435a67e077173783114eba83b4566784c2ac69
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-18 09:18:12 +0200

    create ProfilePage styling

---
+++ src/pages/profile/ProfilePage.styled.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Profile = styled.div``;
+
+export const ProfileForm = styled.form`
+  display: grid;
+`;
+
+export const LogoutButton = styled.button`
+  background-color: ${(props) => props.theme.colors.danger};
+`;
