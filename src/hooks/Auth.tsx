51970b56a582308eb2fd32c00c4af1c1b422afe5
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-18 11:25:26 +0200

    add tons of error handling

--- src/hooks/Auth.tsx
+++ src/hooks/Auth.tsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { useEffect, useState } from "react";
 import { UserData } from "../types/User";
 
@@ -9,16 +9,59 @@ export default function useAuth() {
     JSON.parse(localStorage.getItem("userData") ?? "{}"),
   );
 
+  useEffect(() => {
+    const validateLogin = async () => {
+      try {
+        await axios.get(
+          "http://localhost:8080/api/users/" + loggedInUser?.username,
+        );
+      } catch (e: unknown) {
+        if (e instanceof AxiosError == false) {
+          logout();
+          alert("An unknown error ocurred.");
+          return;
+        }
+        if (e.response?.status == 404) {
+          logout();
+        }
+      }
+    };
+    if (loggedInUser?.username) {
+      validateLogin();
+    }
+  }, [loggedInUser]);
+
   const login = async (
     username: string,
     password: string,
   ): Promise<boolean> => {
-    const res = await axios.post("http://localhost:8080/api/login", {
-      username,
-      password,
-    });
+    let res;
+    try {
+      const axiosResponse = await axios.post(
+        "http://localhost:8080/api/login",
+        {
+          username,
+          password,
+        },
+      );
+      res = axiosResponse;
+    } catch (e: unknown) {
+      if (e instanceof AxiosError == false) {
+        alert("An unknown error ocurred when attempting to log in.");
+        return false;
+      }
+      if (e.response?.status == 404) {
+        alert(`User "${username}" doesn't exist.`);
+        return false;
+      }
+      if (e.response?.status == 401) {
+        alert("Incorrect password. Please try again.");
+        return false;
+      }
+      alert("An unknown error ocurred when attempting to log in.");
+      return false;
+    }
     const newToken = res.data.token;
-    console.log("LOGIN TOKEN:", newToken);
     if (!newToken) {
       alert(
         "An error ocurred when attempting to login. Please try again later.",
@@ -33,6 +76,7 @@ export default function useAuth() {
     );
     setLoggedInUser(userData.data);
     localStorage.setItem("userData", JSON.stringify(userData.data));
+    alert("Login successful!");
     return true;
   };
 
@@ -40,7 +84,9 @@ export default function useAuth() {
     localStorage.removeItem("loginToken");
     setToken(null);
     setIsLoggedIn(false);
+    localStorage.removeItem("userData");
     setLoggedInUser(null);
+    localStorage.removeItem("token");
   };
 
   useEffect(() => {
