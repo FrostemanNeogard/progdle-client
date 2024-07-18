5bb25fe9cc3777710fc17595c65e0eef57cbea8f
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-18 16:18:31 +0200

    improve styling

--- src/pages/game/GamePage.styled.ts
+++ src/pages/game/GamePage.styled.ts
@@ -1,8 +1,16 @@
 import styled from "styled-components";
 
-export const GuessCount = styled.h2``;
+export const GuessCount = styled.h2`
+  font-size: 1.25rem;
+  margin-top: 1rem;
+  text-align: center;
+`;
 
-export const InstructionHeading = styled.p``;
+export const InstructionHeading = styled.p`
+  text-align: center;
+  margin-bottom: 1rem;
+  font-size: 1.75rem;
+`;
 
 export const NavBar = styled.nav`
   width: 100%;
