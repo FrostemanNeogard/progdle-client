453e629e819e0ebb0b26608ab93c7207b8f5ef08
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-18 10:59:24 +0200

    add LoginButton styling

--- src/pages/profile/components/LoginForm.styled.ts
+++ src/pages/profile/components/LoginForm.styled.ts
@@ -3,3 +3,8 @@ import styled from "styled-components";
 export const Form = styled.form`
   display: grid;
 `;
+
+export const LoginButton = styled.button`
+  color: ${(props) => props.theme.colors.tx.secondary};
+  background-color: ${(props) => props.theme.colors.success};
+`;
