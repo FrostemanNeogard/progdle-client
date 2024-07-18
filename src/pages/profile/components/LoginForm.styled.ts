3e5be42a28262860118354f2f7955b67177a7d53
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-19 00:31:46 +0200

    improve LoginForm styling

--- src/pages/profile/components/LoginForm.styled.ts
+++ src/pages/profile/components/LoginForm.styled.ts
@@ -2,6 +2,35 @@ import styled from "styled-components";
 
 export const Form = styled.form`
   display: grid;
+  row-gap: 1rem;
+
+  label {
+    margin-bottom: -0.8rem;
+  }
+
+  input {
+    border-radius: 10px;
+    border: none;
+    padding: 0.5rem;
+    background-color: white;
+    color: ${(props) => props.theme.colors.tx.secondary};
+  }
+
+  button {
+    color: ${(props) => props.theme.colors.tx.secondary};
+    border: none;
+    border-radius: 10px;
+    padding: 1rem;
+
+    &:hover {
+      cursor: pointer;
+      filter: brightness(80%);
+
+      &:disabled {
+        cursor: initial;
+      }
+    }
+  }
 `;
 
 export const LoginButton = styled.button`
