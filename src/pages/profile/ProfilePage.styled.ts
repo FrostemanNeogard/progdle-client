942b2cc8c3f8c3c7b79167204f4396562796c5dc
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-19 00:31:58 +0200

    improve ProfilePage styling

--- src/pages/profile/ProfilePage.styled.ts
+++ src/pages/profile/ProfilePage.styled.ts
@@ -1,11 +1,55 @@
 import styled from "styled-components";
 
-export const Profile = styled.div``;
+export const Profile = styled.div`
+  display: grid;
+  row-gap: 1rem;
+
+  h1 {
+    font-size: 3rem;
+  }
+
+  button {
+    color: ${(props) => props.theme.colors.tx.secondary};
+    border: none;
+    border-radius: 10px;
+    padding: 1rem;
+
+    &:hover {
+      cursor: pointer;
+      filter: brightness(80%);
+
+      &:disabled {
+        cursor: initial;
+      }
+    }
+  }
+`;
 
 export const ProfileForm = styled.form`
   display: grid;
+  row-gap: 1rem;
+
+  label {
+    margin-bottom: -0.8rem;
+  }
+
+  input {
+    border-radius: 10px;
+    border: none;
+    padding: 0.5rem;
+    background-color: white;
+    color: ${(props) => props.theme.colors.tx.secondary};
+  }
 `;
 
 export const LogoutButton = styled.button`
+  background-color: ${(props) => props.theme.colors.danger};
+`;
+
+export const SaveButton = styled.button`
+  background-color: ${(props) => props.theme.colors.success};
+`;
+
+export const CancelButton = styled.button`
   background-color: ${(props) => props.theme.colors.danger};
 `;
