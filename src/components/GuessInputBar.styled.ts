2b2224b0d347b6349b59a18f4232e73c53ec9a41
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-19 00:31:17 +0200

    slightly improve GuessInputBar styling

--- src/components/GuessInputBar.styled.ts
+++ src/components/GuessInputBar.styled.ts
@@ -16,15 +16,20 @@ export const GuessInput = styled.input`
   flex: 1;
   background-color: ${(props) => props.theme.colors.bg.white};
   border-radius: 9999px;
-  padding: 1rem 2rem;
+  padding: 0.8rem 1.8rem;
   color: ${(props) => props.theme.colors.tx.secondary};
 `;
 
 export const GuessButton = styled.button`
   width: 30%;
   background-color: ${(props) => props.theme.colors.success};
-  padding-inline: 1rem;
+  padding-inline: 0.5rem;
   border: none;
   color: ${(props) => props.theme.colors.tx.secondary};
   border-radius: 9999px;
+
+  &:hover {
+    cursor: pointer;
+    filter: brightness(80%);
+  }
 `;
